8213e3c1ed95347f8107bea71a165164959b9141
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-01-05 17:50:03 +0530

    feat : HeroSection added

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./heroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="herosection-container">
+      <div className="herosection-background">
+        <video className="background-video" autoPlay muted loop>
+          <source
+            src="https://video.wixstatic.com/video/9c6671_5fe77d56c59b4643af0145e2c6e50c89/1080p/mp4/file.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+
+      <div className="hero-card-container">
+        <div className="hero-card">
+          <h1 className="hero-card-title">Dunagiri Retreat</h1>
+          <p className="hero-card-description">
+            Experience serenity and divinity steps away from Mahavatar Babaji's
+            Cave in the Himalayas.
+          </p>
+          <button className="hero-card-btn">INQUIRE NOW</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Header from '../../components/header/Header'
+import React from "react";
+import Header from "../../components/header/Header";
+import HeroSection from "../../components/heroSection/HeroSection";
 
 const Home = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <HeroSection/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

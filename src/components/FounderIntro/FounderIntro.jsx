f6967110c06342678aa31a78c7b2c1bdb702a63e
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-01-06 16:51:52 +0530

    style : style change

---
+++ src/components/FounderIntro/FounderIntro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FounderIntro.css'
+
+const FounderIntro = () => {
+  return (
+    <div className='founderintro-container'>
+      
+    </div>
+  )
+}
+
+export default FounderIntro
